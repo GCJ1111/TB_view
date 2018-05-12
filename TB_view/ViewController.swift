4f1cfc40408f3a022100828aabab6650a3264f60
gongchenjie <gongchenjie1@hotmail.com>
2018-05-12 08:32:48 +0800

    Initial Commit

---
+++ TB_view/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TB_view
+//
+//  Created by gongchenjie on 2018/5/12.
+//  Copyright © 2018年 gongchenjie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
